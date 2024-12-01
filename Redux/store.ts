4e20ff85f304c9b677c66bfc79f96dbb25fe4838
Yaron Cohen <yaroncbtv@gmail.com>
2024-12-01 16:53:24 +0200

    add support to clear the cache after 24h

--- Redux/store.ts
+++ Redux/store.ts
@@ -6,6 +6,9 @@ import {combineReducers} from 'redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {booksSaga} from './saga';
 
+const CACHE_PURGE_KEY = 'lastPurgeTimestamp';
+const PURGE_INTERVAL = 24 * 60 * 60 * 1000;
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
@@ -25,10 +28,33 @@ export const store = configureStore({
       serializableCheck: false,
     }).concat(sagaMiddleware),
 });
-sagaMiddleware.run(booksSaga); // Run the root saga
+sagaMiddleware.run(booksSaga);
 
 export const persistor = persistStore(store);
-//persistor.purge();
+
+const checkAndPurgeCache = async () => {
+  try {
+    const lastPurgeTimestamp = await AsyncStorage.getItem(CACHE_PURGE_KEY);
+    const now = Date.now();
+
+    if (
+      !lastPurgeTimestamp ||
+      now - parseInt(lastPurgeTimestamp, 10) >= PURGE_INTERVAL
+    ) {
+      await persistor.purge();
+      console.log('Cache purged after 24 hours.');
+
+      await AsyncStorage.setItem(CACHE_PURGE_KEY, now.toString());
+    } else {
+      console.log('Cache is still valid.');
+    }
+  } catch (error) {
+    console.error('Error checking or purging cache:', error);
+  }
+};
+
+// Run the purge check on app start
+checkAndPurgeCache();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
