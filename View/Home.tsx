12933bac5c5a240d03d32c0519185ef16ca31d46
Yaron Cohen <yaroncbtv@gmail.com>
2024-12-01 21:03:56 +0200

    add dropdown and sort list by a-z & relase date & number pages

--- View/Home.tsx
+++ View/Home.tsx
@@ -6,12 +6,24 @@ import {useAppSelector, useAppDispatch} from '../Redux/hooks';
 import {Book} from '../Interface/Book';
 import {Searchbar} from 'react-native-paper';
 import {useDebounce} from 'use-debounce';
+import {View, Text} from 'react-native';
+import {Menu, Button, Divider, Provider} from 'react-native-paper';
 
 export const Home: React.FC = () => {
   const booksList = useAppSelector(state => state.appSlice.booksList);
   const dispatch = useAppDispatch();
   const [searchQuery, setSearchQuery] = useDebounce('', 500);
   const [filteredItems, setFilteredItems] = React.useState<Book[]>(booksList);
+
+  const [items, setItems] = React.useState([
+    {id: 1, name: 'a-z'},
+    {id: 2, name: 'Number Pages'},
+    {id: 3, name: 'Release Date'},
+  ]);
+
+  const [visible, setVisible] = React.useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
 
   useEffect(() => {
     getData();
@@ -38,7 +50,33 @@ export const Home: React.FC = () => {
       setFilteredItems(filtered);
     }
   };
-
+  const handleSearchSort = (id: number) => {
+    switch (id) {
+      case 1:
+        {
+          const sortedItems = [...booksList].sort((a, b) =>
+            a.title.localeCompare(b.title),
+          );
+          setFilteredItems(sortedItems);
+        }
+        break;
+      case 2:
+        {
+          const sortedItems = booksList.sort((a, b) => a.pages - b.pages);
+          setFilteredItems(sortedItems);
+        }
+        break;
+      case 3:
+        {
+          const sortedItems = booksList.sort(
+            (a, b) => new Date(a.releaseDate) - new Date(b.releaseDate),
+          );
+          setFilteredItems(sortedItems);
+        }
+        break;
+      default:
+    }
+  };
   if (booksList) {
     return (
       <>
@@ -48,6 +86,22 @@ export const Home: React.FC = () => {
           onChangeText={handleSearch}
           value={searchQuery}
         />
+        <View style={{padding: 20}}>
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={<Button onPress={openMenu}>Sort By</Button>}>
+            {items?.map(item => (
+              <Menu.Item
+                key={item.id}
+                title={item.name}
+                onPress={() => handleSearchSort(item.id)}
+              />
+            ))}
+            <Divider />
+            <Menu.Item title="Close" onPress={closeMenu} />
+          </Menu>
+        </View>
         <FlatList
           data={filteredItems}
           keyExtractor={item => item?.number?.toString()}
