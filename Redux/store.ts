b35d7ce095ffcd39f72a46699abfe08432dbbcc6
Yaron Cohen <yaroncbtv@gmail.com>
2024-12-01 16:31:40 +0200

    start to use redux saga

--- Redux/store.ts
+++ Redux/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import appSlice from './appSlice';
 import {persistReducer, persistStore} from 'redux-persist';
 import {combineReducers} from 'redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {booksSaga} from './saga';
 
 const persistConfig = {
   key: 'root',
@@ -14,16 +16,19 @@ const rootReducer = combineReducers({
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(sagaMiddleware),
 });
+sagaMiddleware.run(booksSaga); // Run the root saga
 
 export const persistor = persistStore(store);
+//persistor.purge();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
